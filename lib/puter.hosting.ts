a35addb4e52c5ee30197a63a395b62fa1f4eb605
anuj9631 <anuj98426@gmail.com>
2026-03-02 12:02:45 +0530

    git check

--- lib/puter.hosting.ts
+++ lib/puter.hosting.ts
@@ -1,8 +1,11 @@
 import puter from "@heyputer/puter.js";
+import { HOSTING_CONFIG_KEY } from "./utils";
 
 type HostingConfig = {subdomain : string};
 type HostedAsset = {url : string};
 
 export const grtOrCreateHostingConfig = async () :Promise<HostingConfig | null > =>{
-  const exiting = (await puter.kv.get())
+  const exiting = (await puter.kv.get(HOSTING_CONFIG_KEY)) as HostingConfig |null ;
+
+  
 }
