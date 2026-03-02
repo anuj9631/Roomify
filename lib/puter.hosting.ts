95437fa6fd4300bf62a2ebb4aa22cf6a4ac1d8ae
anuj9631 <anuj98426@gmail.com>
2026-03-02 12:04:56 +0530

    hosting slug

--- lib/puter.hosting.ts
+++ lib/puter.hosting.ts
@@ -6,6 +6,8 @@ type HostedAsset = {url : string};
 
 export const grtOrCreateHostingConfig = async () :Promise<HostingConfig | null > =>{
   const exiting = (await puter.kv.get(HOSTING_CONFIG_KEY)) as HostingConfig |null ;
+  if(exiting ?.subdomain) return {subdomain : exiting.subdomain};
 
-  
+  const subdomain = createHostingSlug();
+
 }
