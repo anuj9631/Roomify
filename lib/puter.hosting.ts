ddd9506d1670c69f8183071b3a0d88c8acd0634b
anuj9631 <anuj98426@gmail.com>
2026-03-02 12:12:38 +0530

    getting or cating subdoomain

--- lib/puter.hosting.ts
+++ lib/puter.hosting.ts
@@ -1,5 +1,5 @@
 import puter from "@heyputer/puter.js";
-import { HOSTING_CONFIG_KEY } from "./utils";
+import { createHoostingSlug, HOSTING_CONFIG_KEY } from "./utils";
 
 type HostingConfig = {subdomain : string};
 type HostedAsset = {url : string};
@@ -8,6 +8,15 @@ export const grtOrCreateHostingConfig = async () :Promise<HostingConfig | null >
   const exiting = (await puter.kv.get(HOSTING_CONFIG_KEY)) as HostingConfig |null ;
   if(exiting ?.subdomain) return {subdomain : exiting.subdomain};
 
-  const subdomain = createHostingSlug();
+  const subdomain = createHoostingSlug();
 
+  try {
+    const created = await puter.hosting.create(subdomain, '.');
+
+return {subdomain: created.subdomain};
+    
+  } catch (e) {
+    console.log(`Could not find subdomain: ${e}`);
+    return null;
+  }
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
 export const HOSTING_CONFIG_KEY =  "roomify_hosting_config";
 
 export const HOSTING_DOMAIN_SUFFIX =  ".puter.site";
+
+export const createHoostingSlug = ()=>`rommify-${Date.now().toString(36)}-${Math.random().toString(36).slice(2,8)}`;
